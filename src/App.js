96314c1f4fc1f25f033214d0c4d643bbf7040298
Heavenjes <paulinecalatrava@gmail.com>
2019-09-28 19:21:02 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [{
+        firstname: "Juan ",
+        lastname: "Dela Cruz ",
+        age: 21
+      },
+      {
+        firstname: "Mark ",
+        lastname: "Paul ",
+        age: 22
+      },
+      {
+        firstname: "Paul ",
+        lastname: "Mark ",
+        age: 23
+      }]
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.data.map(d => <li key={d.firstname}>{d.firstname}{d.lastname}{d.age}</li>)}
+      </div>
+      
+
+    )
+  }
 }
-
 export default App;
